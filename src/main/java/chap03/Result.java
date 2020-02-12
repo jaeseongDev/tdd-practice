82d7e1fd4fa88ccb2c8b7aaa7b25a73f4a4a889b
jaeseongDev <qkrwotjd1445@naver.com>
2020-02-12 11:00:00 +0900

    test

--- src/main/java/chap03/Result.java
+++ src/main/java/chap03/Result.java
@@ -7,5 +7,6 @@ public class Result {
         // Participant가 들고 있는 List<Card>를 사용해야 한다고 가정하자.
         List<Card> participantCards = participant.getCards(); // 디미터 법칙 위반
         // ...
+
     }
 }
