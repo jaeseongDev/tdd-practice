2bd2aefd46219778eb40f8cbbded8cdde2eb666d
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-17 18:22:44 +0900

    동일 상황에 대한 테스트 예를 추가

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -8,12 +8,22 @@ import org.junit.jupiter.api.Test;
 public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
+        // 예 1
         LocalDate billingDate = LocalDate.of(2019, 3, 1);
         int payAmount = 10_000;
 
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
         LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
 
-        assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+        assertEquals(LocalDate.of(2019, 3, 1), expiryDate);
+
+        // 예 2
+        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
+        int payAmount2 = 10_000;
+
+        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
+        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
+
+        assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
     }
 }
