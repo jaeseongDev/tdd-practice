47b6766910b444b119666dc1482e58793eda8dfc
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-17 18:17:35 +0900

    상수를 리턴해서 테스트 통과

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class ExpiryDateCalculatorTest {
     @Test
-    void 만원_납부하면_한달_뒤가_만룡일이_됨() {
+    void 만원_납부하면_한달_뒤가_만료일이_됨() {
         LocalDate billingDate = LocalDate.of(2019, 3, 1);
         int payAmount = 10_000;
 
